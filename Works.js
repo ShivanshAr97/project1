f1175de2ef36207de53eb1ce08dc79677d4f07c3
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-06-29 16:17:33 +0530

    commit

--- Works.js
+++ Works.js
@@ -6,8 +6,8 @@ const Works = ({img, text,num}) => {
         <>
             <div className='flex items-center flex-col w-[33%]'>
                 <Image className='' src={img} width={50} height={50} />
-                <p className='absolute border rounded-lg text-pink-500 text-4xl px-1'>{num}</p>
-                <p className='w-[60%] tracking-wider border rounded-xl my-4 text-2xl text-center'>{text}</p>
+                <p className='absolute border rounded-full bg-red-700 text-white text-[0.7rem] px-[0.30rem] w-4 h-4 -mt-2'>{num}</p>
+                <p className='w-[60%] tracking-wider my-4 text-2xl text-center'>{text}</p>
             </div>
         </>
     )
